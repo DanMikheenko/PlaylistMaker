910af250b94048d65c9eaf77ba6ea2731e770327
Dan Mikheenko <miheenko.denis@gmail.com>
2024-01-17 22:13:24 +0200

    All Toasts have been added

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -3,10 +3,11 @@ package com.practicum.playlistmaker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,5 +18,24 @@ class MainActivity : AppCompatActivity() {
             }
         }
         searchButton.setOnClickListener(buttonClickListener)
+        val settingButton = findViewById<Button>(R.id.setting_button)
+        val settingButtonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку настроек!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        settingButton.setOnClickListener(settingButtonClickListener)
+
+
+        onClick(findViewById<Button>(R.id.media_button))
+
+
+    }
+
+    override fun onClick(p0: View?) {
+        //val mediaButton = findViewById<Button>(R.id.media_button)
+        p0?.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Нажали на кнопку медиатеки!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
