941316eb99176e442c13839b55d8c75187d9b93d
Dan Mikheenko <miheenko.denis@gmail.com>
2024-01-19 23:55:57 +0200

    Unused imports have been removed

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(), OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/practicum/playlistmaker/SettingActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingActivity.kt
@@ -1,9 +1,8 @@
 package com.practicum.playlistmaker
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
