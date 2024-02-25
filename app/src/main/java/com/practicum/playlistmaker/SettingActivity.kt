a515cc7902dbf29748bb9b39e39b7ab7cad46df8
Dan Mikheenko <miheenko.denis@gmail.com>
2024-02-25 22:56:15 +0200

    share functions have been added

--- app/src/main/java/com/practicum/playlistmaker/SettingActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingActivity.kt
@@ -1,5 +1,6 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -14,5 +15,24 @@ class SettingActivity : AppCompatActivity() {
             finish()
         }
 
+        val shareTextView = findViewById<View>(R.id.shareTextView)
+        shareTextView.setOnClickListener{
+            share()
+        }
+
+    }
+    private fun share() {
+
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+
+
+        var shareMessage = "https://practicum.yandex.ru/android-developer/"
+
+        // Устанавливаем текст сообщения
+        shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage)
+
+        // Запускаем диалог шаринга
+        startActivity(Intent.createChooser(shareIntent, "Поделиться приложением через..."))
     }
 }
