620c030124b22c36afa78a5a1e284b8a35879fc1
Dan Mikheenko <miheenko.denis@gmail.com>
2024-03-02 22:20:29 +0200

    added SearchActivity

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,29 @@
 package com.practicum.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val homeButton = findViewById<View>(R.id.searchTextView)
+        homeButton.setOnClickListener{
+            finish()
+        }
+        val buttonClear = findViewById<View>(R.id.btnClear)
+        buttonClear.setOnClickListener {
+            resetSearchText()
+        }
+    }
+
+
+
+    private fun resetSearchText() {
+        val editText = findViewById<EditText>(R.id.editText)
+        editText.text.clear()
     }
 }
