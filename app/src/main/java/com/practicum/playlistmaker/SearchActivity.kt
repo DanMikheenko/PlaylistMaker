6df7fc39296235cfc6ae889c6bdb52f417dceb72
Dan Mikheenko <miheenko.denis@gmail.com>
2024-03-05 17:47:37 +0200

    fixed cancelBtn

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,9 +1,11 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
@@ -22,6 +24,7 @@ class SearchActivity : AppCompatActivity() {
         val buttonClear = findViewById<View>(R.id.btnClear)
         buttonClear.setOnClickListener {
             resetSearchText()
+            hideKeyboard()
         }
 
         editText.addTextChangedListener(object : TextWatcher {
@@ -53,5 +56,10 @@ class SearchActivity : AppCompatActivity() {
         val currentText = editText.text.toString()
         outState.putString(KEY_EDIT_TEXT, currentText)
         super.onSaveInstanceState(outState)
+    }
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
     }
 }
