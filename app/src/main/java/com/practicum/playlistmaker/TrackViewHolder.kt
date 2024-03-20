d787ba9850bdf9571499be67414cb71cd38404fb
Dan Mikheenko <miheenko.denis@gmail.com>
2024-03-20 13:01:34 +0200

    Final commit

--- app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
@@ -5,8 +5,9 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
-class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)  {
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val trackName: TextView = itemView.findViewById(R.id.trackName)
     private val trackImage: ImageView = itemView.findViewById(R.id.trackImage)
     private val artistName: TextView = itemView.findViewById(R.id.artistName)
@@ -14,7 +15,8 @@ class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)  {
 
     fun bind(model: Track) {
         trackName.text = model.trackName
-        Glide.with(trackImage).load(model.artworkUrl100).placeholder(R.drawable.ic_track_image_placeholder_background).into(trackImage)
+        Glide.with(trackImage).load(model.artworkUrl100).centerCrop().transform(RoundedCorners(10))
+            .placeholder(R.drawable.ic_track_image_placeholder_background).into(trackImage)
         artistName.text = model.artistName
         trackTime.text = model.trackTime
     }
