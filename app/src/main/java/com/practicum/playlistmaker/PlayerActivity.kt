627aa06ffb92d26e34d5ff44fd04e21b6ef8e083
Dan Mikheenko <miheenko.denis@gmail.com>
2024-04-17 00:39:27 +0200

    added binder

---
+++ app/src/main/java/com/practicum/playlistmaker/PlayerActivity.kt
@@ -0,0 +1,58 @@
+package com.practicum.playlistmaker
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.gson.Gson
+
+class PlayerActivity() : AppCompatActivity() {
+
+    lateinit var track: Track
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+        val intent = intent
+        var selectedTrackJson = intent.getStringExtra("selectedTrack")
+        track = Gson().fromJson<Track>(selectedTrackJson, Track::class.java)
+
+        val trackImagePlayer: ImageView = findViewById(R.id.trackImagePlayer)
+        Glide.with(trackImagePlayer)
+            .load(track.getCoverArtwork())
+            .placeholder(R.drawable.player_image_placeholder)
+            .error(R.drawable.player_image_placeholder)
+            .into(trackImagePlayer)
+
+
+        val trackName = findViewById<TextView>(R.id.trackNamePlayer)
+        trackName.text = track.trackName
+
+        val trackAuthor = findViewById<TextView>(R.id.trackAuthor)
+        trackAuthor.text = track.artistName
+
+        val trackDurationTextView = findViewById<TextView>(R.id.trackDurationTextView)
+        trackDurationTextView.text = track.formattedDuration()
+
+        val albNameTextView = findViewById<TextView>(R.id.albNameTextView)
+        albNameTextView.text = track.collectionName
+
+        val yearTextView = findViewById<TextView>(R.id.yearTextView)
+        yearTextView.text = track.releaseDate
+
+        val genreTextView = findViewById<TextView>(R.id.genreTextView)
+        genreTextView.text = track.primaryGenreName
+
+        val countryTextView = findViewById<TextView>(R.id.countryTextView)
+        countryTextView.text = track.country
+
+
+        val playerBackBtn = findViewById<TextView>(R.id.playerBackButton)
+        playerBackBtn.setOnClickListener {
+            finish()
+        }
+    }
+
+
+}
