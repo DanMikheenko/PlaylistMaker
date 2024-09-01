2f4bcd9044488f33eda4bd9e50aea27a7d111472
Dan Mikheenko <miheenko.denis@gmail.com>
2024-09-01 20:46:06 +0300

    fix: added isSuccessful method to Response

--- app/src/main/java/com/practicum/playlistmaker/data/TracksRepositoryImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/TracksRepositoryImpl.kt
@@ -1,15 +1,15 @@
 package com.practicum.playlistmaker.data
 
-import com.practicum.playlistmaker.domain.models.Track
 import com.practicum.playlistmaker.data.dto.TracksSearchRequest
 import com.practicum.playlistmaker.data.dto.TracksSearchResponse
 import com.practicum.playlistmaker.domain.api.TracksRepository
+import com.practicum.playlistmaker.domain.models.Track
 
 class TracksRepositoryImpl(private val networkClient: NetworkClient) : TracksRepository {
 
     override fun searchTracks(expression: String): List<Track> {
         val response = networkClient.doRequest(TracksSearchRequest(expression))
-        if (response.resultCode == 200) {
+        if (response.isSuccessful()) {
             return (response as TracksSearchResponse).results.map {
                 Track(
                     it.trackId,
--- app/src/main/java/com/practicum/playlistmaker/data/dto/Response.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/dto/Response.kt
@@ -2,4 +2,11 @@ package com.practicum.playlistmaker.data.dto
 
 open class Response() {
     var resultCode = 0
+    fun isSuccessful(): Boolean {
+        if (resultCode==200){
+            return true
+        } else{
+            return false
+        }
+    }
 }
