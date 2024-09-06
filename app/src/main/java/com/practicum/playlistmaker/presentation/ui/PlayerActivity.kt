ed6d7ab357a758c1dab5dacb4cbe1522790f7d69
Dan Mikheenko <miheenko.denis@gmail.com>
2024-09-07 00:51:12 +0300

    fix: player has been refactored according to the clean architecture

--- app/src/main/java/com/practicum/playlistmaker/presentation/ui/PlayerActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/presentation/ui/PlayerActivity.kt
@@ -2,7 +2,6 @@ package com.practicum.playlistmaker.presentation.ui
 
 import android.content.res.Configuration
 import android.icu.text.SimpleDateFormat
-import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -12,42 +11,45 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.google.gson.Gson
+import com.practicum.playlistmaker.Creator
 import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.domain.api.PlayerInteractor
 import com.practicum.playlistmaker.domain.models.Track
 import java.util.Locale
 
-class PlayerActivity() : AppCompatActivity() {
-
-    companion object {
-        private const val SELECTED_TRACK = "selectedTrack"
-        private const val STATE_DEFAULT = 0
-        private const val STATE_PREPARED = 1
-        private const val STATE_PLAYING = 2
-        private const val STATE_PAUSED = 3
-    }
+class PlayerActivity : AppCompatActivity() {
 
     private var playerState = STATE_DEFAULT
+    private lateinit var interactor: PlayerInteractor
     private lateinit var track: Track
-    private var mediaPlayer = MediaPlayer()
-    private lateinit var play: TextView
-    private var mainThreadHandler: Handler? = Handler(Looper.getMainLooper())
+    private lateinit var playButton: TextView
     private lateinit var secondsLeftTextView: TextView
+    private var mainThreadHandler: Handler? = Handler(Looper.getMainLooper())
     private var runnable: Runnable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
 
+        interactor = Creator.providePlayerInteractor()
+
         val intent = intent
         val selectedTrackJson = intent.getStringExtra(SELECTED_TRACK)
-        track = Gson().fromJson<Track>(selectedTrackJson, Track::class.java)
-        play = findViewById(R.id.playButton)
-        preparePlayer()
-        play.setOnClickListener {
-            playbackControl()
-        }
+        track = Gson().fromJson(selectedTrackJson, Track::class.java)
+
+        playButton = findViewById(R.id.playButton)
         secondsLeftTextView = findViewById(R.id.trackTimeMillisTextView)
 
+        preparePlayer()
+
+        playButton.setOnClickListener {
+            playbackControl()
+        }
+
+        setupUI()
+    }
+
+    private fun setupUI() {
         val trackImagePlayer: ImageView = findViewById(R.id.trackImagePlayer)
         Glide.with(trackImagePlayer)
             .load(track.getCoverArtwork())
@@ -56,84 +58,44 @@ class PlayerActivity() : AppCompatActivity() {
             .error(R.drawable.player_image_placeholder)
             .into(trackImagePlayer)
 
-        val trackName = findViewById<TextView>(R.id.trackNamePlayer)
-        trackName.text = track.trackName
+        findViewById<TextView>(R.id.trackNamePlayer).text = track.trackName
+        findViewById<TextView>(R.id.trackAuthor).text = track.artistName
+        findViewById<TextView>(R.id.trackDurationTextView).text = track.formattedDuration()
+        findViewById<TextView>(R.id.albNameTextView).text = track.collectionName
+        findViewById<TextView>(R.id.yearTextView).text = track.releaseDate?.substring(0, 4)
+        findViewById<TextView>(R.id.genreTextView).text = track.primaryGenreName
+        findViewById<TextView>(R.id.countryTextView).text = track.country
 
-        val trackAuthor = findViewById<TextView>(R.id.trackAuthor)
-        trackAuthor.text = track.artistName
-
-        val trackDurationTextView = findViewById<TextView>(R.id.trackDurationTextView)
-        trackDurationTextView.text = track.formattedDuration()
-
-        val albNameTextView = findViewById<TextView>(R.id.albNameTextView)
-        albNameTextView.text = track.collectionName
-
-        val yearTextView = findViewById<TextView>(R.id.yearTextView)
-        yearTextView.text = track.releaseDate?.substring(0, 4)
-
-        val genreTextView = findViewById<TextView>(R.id.genreTextView)
-        genreTextView.text = track.primaryGenreName
-
-        val countryTextView = findViewById<TextView>(R.id.countryTextView)
-        countryTextView.text = track.country
-
-
-        val playerBackBtn = findViewById<TextView>(R.id.playerBackButton)
-        playerBackBtn.setOnClickListener {
+        findViewById<TextView>(R.id.playerBackButton).setOnClickListener {
             finish()
         }
     }
 
     private fun preparePlayer() {
-        mediaPlayer.setDataSource(track.previewUrl)
-        mediaPlayer.prepareAsync()
-        mediaPlayer.setOnPreparedListener {
-            play.isEnabled = true
-            playerState = STATE_PREPARED
-        }
-        mediaPlayer.setOnCompletionListener {
-            playerState = STATE_PREPARED
-            runnable?.let { mainThreadHandler?.removeCallbacks(it) }
-            secondsLeftTextView.text = "00:00"
-
-            //todo: Это должно решаться через Resource Qualifiers
-            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-                Configuration.UI_MODE_NIGHT_YES -> {
-                    play.setBackgroundResource(R.drawable.play_button_dark)
-                }
-                Configuration.UI_MODE_NIGHT_NO -> {
-                    play.setBackgroundResource(R.drawable.play_button)
-                }
-                Configuration.UI_MODE_NIGHT_UNDEFINED -> {
-                    play.setBackgroundResource(R.drawable.play_button)
-                }
+        interactor.preparePlayer(
+            track,
+            onPrepared = {
+                playButton.isEnabled = true
+            },
+            onCompletion = {
+                playerState = STATE_PREPARED
+                runnable?.let { mainThreadHandler?.removeCallbacks(it) }
+                secondsLeftTextView.text = "00:00"
+                updatePlayButtonBackground()
             }
-        }
+        )
     }
 
     private fun startPlayer() {
-        mediaPlayer.start()
-
-        //todo: Это должно решаться через Resource Qualifiers
-        // Проверка текущей темы
-        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-            Configuration.UI_MODE_NIGHT_YES -> {
-                play.setBackgroundResource(R.drawable.pause_button_image_dark_theme)
-            }
-            Configuration.UI_MODE_NIGHT_NO -> {
-                play.setBackgroundResource(R.drawable.pause_button_image_light_theme)
-            }
-            Configuration.UI_MODE_NIGHT_UNDEFINED -> {
-                play.setBackgroundResource(R.drawable.pause_button_image_light_theme)
-            }
-        }
+        interactor.startPlayer()
+        updatePauseButtonBackground()
 
         runnable = object : Runnable {
             override fun run() {
                 secondsLeftTextView.text = SimpleDateFormat(
                     "mm:ss",
                     Locale.getDefault()
-                ).format(mediaPlayer.currentPosition)
+                ).format(interactor.getCurrentPosition())
                 mainThreadHandler?.postDelayed(this, 10)
             }
         }
@@ -142,30 +104,47 @@ class PlayerActivity() : AppCompatActivity() {
     }
 
     private fun pausePlayer() {
-        mediaPlayer.pause()
-
-        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-            Configuration.UI_MODE_NIGHT_YES -> {
-                play.setBackgroundResource(R.drawable.play_button_dark)
-            }
-            Configuration.UI_MODE_NIGHT_NO -> {
-                play.setBackgroundResource(R.drawable.play_button)
-            }
-            Configuration.UI_MODE_NIGHT_UNDEFINED -> {
-                play.setBackgroundResource(R.drawable.play_button)
-            }
-        }
+        interactor.pausePlayer()
+        updatePlayButtonBackground()
         playerState = STATE_PAUSED
     }
 
     private fun playbackControl() {
         when (playerState) {
-            STATE_PLAYING -> {
-                pausePlayer()
+            STATE_PLAYING -> pausePlayer()
+            STATE_PREPARED, STATE_PAUSED -> startPlayer()
+            STATE_DEFAULT -> startPlayer()
+        }
+    }
+
+    private fun updatePlayButtonBackground() {
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                playButton.setBackgroundResource(R.drawable.play_button_dark)
             }
 
-            STATE_PREPARED, STATE_PAUSED -> {
-                startPlayer()
+            Configuration.UI_MODE_NIGHT_NO -> {
+                playButton.setBackgroundResource(R.drawable.play_button)
+            }
+
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                playButton.setBackgroundResource(R.drawable.play_button)
+            }
+        }
+    }
+
+    private fun updatePauseButtonBackground() {
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                playButton.setBackgroundResource(R.drawable.pause_button_image_dark_theme)
+            }
+
+            Configuration.UI_MODE_NIGHT_NO -> {
+                playButton.setBackgroundResource(R.drawable.pause_button_image_light_theme)
+            }
+
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                playButton.setBackgroundResource(R.drawable.pause_button_image_light_theme)
             }
         }
     }
@@ -177,7 +156,15 @@ class PlayerActivity() : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        mediaPlayer.release()
+        interactor.releasePlayer()
         runnable?.let { mainThreadHandler?.removeCallbacks(it) }
+    }
+
+    companion object {
+        private const val SELECTED_TRACK = "selectedTrack"
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
     }
 }
