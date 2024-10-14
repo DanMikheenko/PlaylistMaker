0826657e10eb978c6ad3d94ebe24ef1e9bd5ce4f
Dan Mikheenko <miheenko.denis@gmail.com>
2024-10-14 23:53:19 +0300

    fix: fixed bug with theme settings

--- app/src/main/java/com/practicum/playlistmaker/creator/App.kt
+++ app/src/main/java/com/practicum/playlistmaker/creator/App.kt
@@ -4,19 +4,22 @@ import android.app.Application
 import com.practicum.playlistmaker.di.dataModule
 import com.practicum.playlistmaker.di.interactorModule
 import com.practicum.playlistmaker.di.viewModelModule
+import com.practicum.playlistmaker.settings.domain.api.ThemeSettingsInteractor
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 import org.koin.core.context.startKoin
 
-class App: Application() {
+class App: Application(), KoinComponent{
+
     override fun onCreate() {
         super.onCreate()
-//        Creator.setApplicationContext(this)
-//        val themeSettingsInteractor = Creator.provideThemeSettingsInteractor()
-//        themeSettingsInteractor.getInitialTheme()
-
         startKoin {
             androidContext(this@App)
             modules(dataModule, interactorModule, viewModelModule)
         }
+
+        val themeSettingsInteractor: ThemeSettingsInteractor by inject()
+        themeSettingsInteractor.getInitialTheme()
     }
 }
