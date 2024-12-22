a0bb835491415a4efc910d9657fcf4f98f06a9ae
Dan Mikheenko <miheenko.denis@gmail.com>
2024-12-22 22:06:56 +0200

    fix: fixed presentation layer

--- app/src/main/java/com/practicum/playlistmaker/media_library/ui/view_model/FavoriteTracksViewModel.kt
+++ app/src/main/java/com/practicum/playlistmaker/media_library/ui/view_model/FavoriteTracksViewModel.kt
@@ -3,12 +3,24 @@ package com.practicum.playlistmaker.media_library.ui.view_model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.practicum.playlistmaker.media_library.domain.api.FavoriteTracksInteractor
+import kotlinx.coroutines.launch
 
-class FavoriteTracksViewModel : ViewModel() {
+class FavoriteTracksViewModel(private val favoriteTracksInteractor: FavoriteTracksInteractor) :
+    ViewModel() {
     private val _state = MutableLiveData<FavoriteTracksState>()
     val state: LiveData<FavoriteTracksState> get() = _state
 
-    fun loadData(){
-        _state.postValue(FavoriteTracksState.ShowPlaceholder)
+    fun loadData() {
+        viewModelScope.launch {
+            val favoriteTracks = favoriteTracksInteractor.getAllFavoriteTracks()
+            favoriteTracks.collect() { tracks ->
+                if (tracks.isEmpty()) _state.postValue(FavoriteTracksState.ShowPlaceholder)
+                else {
+                    _state.postValue(FavoriteTracksState.ShowFavoriteTracks(tracks))
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/player/ui/view_model/PlayerViewModel.kt
+++ app/src/main/java/com/practicum/playlistmaker/player/ui/view_model/PlayerViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.practicum.playlistmaker.media_library.domain.api.FavoriteTracksInteractor
 import com.practicum.playlistmaker.player.domain.api.PlayerInteractor
 import com.practicum.playlistmaker.search.domain.models.Track
 import kotlinx.coroutines.Job
@@ -12,7 +13,8 @@ import kotlinx.coroutines.launch
 
 class PlayerViewModel(
     private val playerInteractor: PlayerInteractor,
-    private val track: Track
+    private val track: Track,
+    private val favoriteTracksInteractor: FavoriteTracksInteractor
 ) :
     ViewModel() {
 
@@ -21,6 +23,9 @@ class PlayerViewModel(
 
     private val _playingTrackPosition = MutableLiveData<Int>()
     val playingTrackPosition: LiveData<Int> = _playingTrackPosition
+
+    private val _isFavoriteTrack = MutableLiveData<Boolean>()
+    val isFavoriteTrack : LiveData<Boolean> = _isFavoriteTrack
 
     private var playbackJob: Job? = null
 
@@ -79,6 +84,22 @@ class PlayerViewModel(
     override fun onCleared() {
         super.onCleared()
         stopTrackingPlayingTrackPosition()
+    }
+
+    fun onFavoriteClicked(){
+        if (track.isFavorite) {
+            viewModelScope.launch {
+                favoriteTracksInteractor.removeTrackFromFavoriteList(track)
+                track.isFavorite = false
+                _isFavoriteTrack.postValue(false)
+            }
+        } else{
+            viewModelScope.launch {
+                favoriteTracksInteractor.addTrackToFavoriteList(track)
+                track.isFavorite = true
+                _isFavoriteTrack.postValue(true)
+            }
+        }
     }
     companion object{
         private const val CLICK_DEBOUNCE_DELAY = 300L
