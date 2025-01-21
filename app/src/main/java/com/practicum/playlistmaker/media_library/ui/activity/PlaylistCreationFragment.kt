b24f50eef3e5ec1d3a0ed30ccb52b65f9a5b2e52
Dan Mikheenko <miheenko.denis@gmail.com>
2025-01-21 23:28:09 +0200

    fix: added photo selection function

--- app/src/main/java/com/practicum/playlistmaker/media_library/ui/activity/PlaylistCreationFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/media_library/ui/activity/PlaylistCreationFragment.kt
@@ -1,14 +1,23 @@
 package com.practicum.playlistmaker.media_library.ui.activity
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.practicum.playlistmaker.MainActivity
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.FragmentPlaylistCreationBinding
+import java.io.File
+import java.io.FileOutputStream
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -41,7 +50,43 @@ class PlaylistCreationFragment : Fragment() {
             findNavController().navigate(R.id.action_playlistCreationFragment_to_tabContainerFragment)
         }
         (activity as? MainActivity)?.hideBottomNav()
+
+        val pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                //обрабатываем событие выбора пользователем фотографии
+                if (uri != null) {
+                    binding.imageView.setImageURI(uri)
+                    saveImageToPrivateStorage(uri)
+                } else {
+                    Log.d("PhotoPicker", "No media selected")
+                }
+            }
+
+        binding.imageView.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
+
     }
+    private fun saveImageToPrivateStorage(uri: Uri) {
+        //создаём экземпляр класса File, который указывает на нужный каталог
+        val filePath = File(requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "myalbum")
+        //создаем каталог, если он не создан
+        if (!filePath.exists()){
+            filePath.mkdirs()
+        }
+        //создаём экземпляр класса File, который указывает на файл внутри каталога
+        val file = File(filePath, "first_cover.jpg")
+        // создаём входящий поток байтов из выбранной картинки
+        val inputStream = requireContext().contentResolver.openInputStream(uri)
+        // создаём исходящий поток байтов в созданный выше файл
+        val outputStream = FileOutputStream(file)
+        // записываем картинку с помощью BitmapFactory
+        BitmapFactory
+            .decodeStream(inputStream)
+            .compress(Bitmap.CompressFormat.JPEG, 30, outputStream)
+    }
+
 
     override fun onPause() {
         super.onPause()
